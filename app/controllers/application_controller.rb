90975e83687d2e0c3a2a5b6fa775ad9b9a289e58
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-24 06:36:49 +0500

    rails app: add route home and pages/home.html.erb

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+ 
+end
+
+# def hello
+#   render html: "Hello World!"
+# end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'pages#home'
+end
+
+# 2 send it to pages
+# root 'application#hello'
+# 1
+# root 'application'
+# means it goes to 
+# /app/controllers/application_controller.rb
