a1bb8eb0f408812213ec0ca7388cde1cb6e14156
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-28 07:17:07 +0500

    add email max 105, presence true (user.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: {minimum: 3, maximum: 25}
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, length: { maximum: 105 }
 end
