d9e8d9179004f8e56d4ece0d2383bf786f09b25d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-28 07:44:33 +0500

    add check correct email with regex (user.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :email, presence: true, 
             uniqueness: { case_sensitive: false },
-            length: { maximum: 105 }
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 end
 
 # uniqueness: true
