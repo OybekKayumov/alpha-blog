350a3de6dcb0e1dd5a4cc227d03d37ed3dcaa3c0
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-28 07:30:13 +0500

    add check uniqueness with case_sensitive (user.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum: 25 }
-  validates :email, presence: true, length: { maximum: 105 }
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 }
 end
+
+# uniqueness: true
