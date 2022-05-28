f2d0356429ac24c84d24bb5619a83410532087f2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-28 12:04:34 +0500

    add user_id column in articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_28_004304) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_28_065608) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
