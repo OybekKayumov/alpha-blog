250c792245f93d4a8e2fe009cde26fa5f528d692
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-29 05:00:49 +0500

    add users create action and update create-user card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  # acrion create
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alhha Blog #{@user.username}, you have successfully signed   up "
+      redirect_to articles_path
+    else 
+      render 'new'
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
