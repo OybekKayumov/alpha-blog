7702d7bd12acf5ca62a170652c9b54ad1057b536
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-06-04 21:33:32 +0500

    create user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "Logged In Successfully"
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render "new"
+    end
   end
   
   def destroy    
